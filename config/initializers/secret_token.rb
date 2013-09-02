8a197bc6f1b02ff5845d53e1c1ed7e58414549ac
mskubenich <misha.skubenich@gmail.com>
2013-09-02 21:53:59 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kotelok::Application.config.secret_token = '83275ec4896ec57729f118aba35937bebf3ed2a5bd3b1a0185dc0c555698cc94d705b9750aa2aa26d4391d02c7be4da43c6a10b6be56cdf9871537d7dc5dac74'
