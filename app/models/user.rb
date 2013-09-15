1dd3bd726b79d428dee046a493b4fe3dae919ff4
mskubenich <misha.skubenich@gmail.com>
2013-09-15 14:49:19 +0300

    update profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :email, :password, :remember_me
+  devise :database_authenticatable, :registerable,
+         :recoverable, :trackable, :validatable,
+         :token_authenticatable
+  attr_accessible :email, :password, :remember_me, :password_confirmation
 end
