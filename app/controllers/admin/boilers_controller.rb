fd167f711fdcee8c25a6e3efd165f791537a1cb3
mskubenich <misha.skubenich@gmail.com>
2013-10-05 13:32:56 +0300

    sign up page design

--- app/controllers/admin/boilers_controller.rb
+++ app/controllers/admin/boilers_controller.rb
@@ -15,7 +15,7 @@ class Admin::BoilersController < Admin::AdminController
     @boiler = Boiler.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html # new.html.haml
       format.json { render json: @boiler }
     end
   end
