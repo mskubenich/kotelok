ee82c11fda3cc3fc0acb5703a636f987efae6c68
mskubenich <misha.skubenich@gmail.com>
2013-10-05 13:36:30 +0300

    disable registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :trackable, :validatable,
          :token_authenticatable
   attr_accessible :email, :password, :remember_me, :password_confirmation, :phone, :address
