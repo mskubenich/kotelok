b3436c82eb77e82f3eacf83dc52d72547ce75ed5
arthur <worldintime@gmail.com>
2013-11-03 13:26:12 +0200

    meta tags and titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,20 @@ module ApplicationHelper
     PricePage.first
   end
 
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      content_for?(:meta_keywords) ? content_for(:meta_keywords) : ''
+    end
+  end
+
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : ''
+    end
+  end
+
 end
