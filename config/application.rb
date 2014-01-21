c448f553d4b71cdd5ce3e19586c806511430596f
worldintime <worldintime@gmail.com>
2014-01-21 13:43:41 +0200

    google gem

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Kotelok
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    if Rails.env == "production"
+      config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-47322477-1")
+    end
   end
 end
